bc810ff3c357e0c3fb63af32f70b4619fc552eb6
ouidane <ouidanezakaria@gmail.com>
2024-02-08 04:58:31 +0100

    edit

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -85,7 +85,7 @@ export default function Intro() {
       >
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/zakaria-ouidane-a6a199272/"
           target="_blank"
         >
           <BsLinkedin />
@@ -93,7 +93,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/ouidane"
           target="_blank"
         >
           <FaGithubSquare />
